2a96d2e4cab53f91e9b93c135b42b86a9d17bbcc
rafaellindemann <rafaelld@gmail.com>
2023-09-14 19:33:50 -0300

    Commit inicial do windows 15 para celulares

---
+++ windows15/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Eu sou o windows para celular</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
